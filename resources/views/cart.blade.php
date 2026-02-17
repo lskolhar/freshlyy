b725ea3f0d7ccf623bbac344fe9a5c894a8a050d
Lakshmi <lskolhar@gmail.com>
2026-02-17 13:01:24 +0530

    Add Cancel button to cart view for improved user experience

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -74,7 +74,10 @@
                         class="bg-green-400 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg shadow-md transition duration-200">
                         Pay
                     </button>
-                    
+                    <button type="submit"
+                        class="bg-red-400 hover:bg-red-600 text-white font-semibold px-6 py-3 rounded-lg shadow-md transition duration-200">
+                        Cancel
+                    </button>
                 </form>
             </div>
         @endif
